39440b82148b91c0ed406a4e78d439961dd7abbd
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-03-25 18:18:47 +0900

    Initial Commit

---
+++ WebtoonUIUX/WebtoonUIUXApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebtoonUIUXApp.swift
+//  WebtoonUIUX
+//
+//  Created by 이윤지 on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WebtoonUIUXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
